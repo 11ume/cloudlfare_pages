799fdd51232c70afc8eda714275ddb9b8c71d326
horacio <horacio.rivero@educabot.com>
2023-06-07 03:45:45 -0300

    last changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
-import { sentryVitePlugin } from '@sentry/vite-plugin'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const isDev = mode === 'development'
@@ -30,14 +28,6 @@ export default ({ mode }) => {
     },
     plugins: [
       react(),
-      // sentryVitePlugin({
-      //   org: 'educabot',
-      //   project: 'aula',
-      //   include: './build',
-      //   authToken: env.VITE_SENTRY_AUTH_TOKEN,
-      //   release: env.APP_RELEASE,
-      //   telemetry: false,
-      // }),
     ],
   })
 }
